b3575b1fa964df1a4f19fc1a86ffe48afd76571c
okunoryo <okuno.ryo.411@gmail.com>
2015-10-29 19:09:36 +0900

    1st

---
+++ lib/actor/human.rb
@@ -0,0 +1,14 @@
+module Actor
+  class Human
+    def initialize(message="nothing")
+      @message = message
+    end
+    def show_message
+      @message
+    end
+    def show_message_length
+      @message.length
+    end
+  end
+end
+
---
+++ main.rb
@@ -0,0 +1,5 @@
+require "./lib/actor/human"
+
+actor = Actor::Human.new 'My Name Is Hogehone'
+
+puts actor.show_message
